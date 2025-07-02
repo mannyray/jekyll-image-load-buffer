c814f7518bd1ed4c34ad06e658576574ac8b7f13
Stan Z <nil>
2025-07-02 16:01:46 -0500

    basic code to simulate slowly loading images

---
+++ test_backend/README.py
@@ -0,0 +1,8 @@
+# How to run flask app
+
+```
+python3 -m venv /tmp/tmpenv
+source /tmp/tmpenv/bin/activate
+python3 -m pip install flask
+python3 app.py
+```
---
+++ test_backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, send_from_directory, abort
+import os
+import time
+
+app = Flask(__name__)
+
+# Folder where images are stored
+IMAGE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')
+
+# Delay time in seconds (you can change this value)
+RESPONSE_DELAY_SECONDS = 2  # 2-second delay
+
+@app.route('/images/<path:filename>')
+def serve_image(filename):
+    time.sleep(RESPONSE_DELAY_SECONDS)  # Add artificial lag
+
+    try:
+        return send_from_directory(IMAGE_FOLDER, filename)
+    except FileNotFoundError:
+        abort(404)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=3000, debug=True)
